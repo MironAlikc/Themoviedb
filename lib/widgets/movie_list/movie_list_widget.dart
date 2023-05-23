65d62cd185a5d9d7ee86308b1c920106561c52fc
MironAlikc <miron.alikc@gmail.com>
2023-05-23 12:55:32 +0300

    movie list

---
+++ lib/widgets/movie_list/movie_list_widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:themoviedb/resources/app_images.dart';
+
+class MovieListWidget extends StatelessWidget {
+  const MovieListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemExtent: 163,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black.withOpacity(0.2)),
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 8,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: const Row(
+                  children: [
+                    Image(
+                      image: AssetImage(AppImages.luca),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 20),
+                          Text(
+                            'Падение Луны',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            'Февраль 04, 2022',
+                            style: TextStyle(color: Colors.grey),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'В 2011 году во время рядового ремонта спутника на находившихся',
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                  ],
+                ),
+              ),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(10),
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
